e42336dbe7cad8798db10bb40ba1cbff2dbb83f3
Pawel Wieczorek <pawelwieczorek98@gmail.com>
2023-10-22 22:53:15 +0200

    push 4

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+
+const fetchCountries = name => {
+    // console.log("fetchCountries, przekazane name:", name);
+    return fetch(
+      `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`,
+    )
+      .then(response => {
+        console.log('fetchCountries response', response);
+        if (!response.ok) {
+          throw new Error('fetchCountries response error', response.status);
+        }
+        return response.json();
+      })
+
+  }
+  
+  export { fetchCountries };
